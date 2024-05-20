7268405329add44a5dfbade2ad02c333ef3df0c2
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2024-05-19 20:08:34 -0500

    remove env

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,19 +1,10 @@
-import "./src/env.mjs";
-import "@crossfit-velo/api/env";
-import "@crossfit-velo/stripe/env";
-
 import withMDX from "@next/mdx";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [
-    "@crossfit-velo/api",
-    "@crossfit-velo/db",
-    "@crossfit-velo/stripe",
-    "@crossfit-velo/ui",
-  ],
+  transpilePackages: ["@crossfit-velo/ui"],
   pageExtensions: ["ts", "tsx", "mdx"],
   experimental: {
     mdxRs: true,
